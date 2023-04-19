77b9d488e8ab7dabfe62e4479209cd9cc921e418
Ramya.U <ramyayadav2016@gmail.com>
2023-04-19 21:35:54 +0530

    seeding category table through migrations

--- BookBloom/Data/BookBloomDbContext.cs
+++ BookBloom/Data/BookBloomDbContext.cs
@@ -11,5 +11,16 @@ namespace BookBloom.Data
         }
 
         public DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci-fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
     }
+
 }
