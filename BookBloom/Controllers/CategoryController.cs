6cd03ec17d1eff831568eeb8dedb768100e282a0
Ramya.U <ramyayadav2016@gmail.com>
2023-04-23 20:32:46 +0530

    adding edit and delete button and adding get method category controller

--- BookBloom/Controllers/CategoryController.cs
+++ BookBloom/Controllers/CategoryController.cs
@@ -37,10 +37,10 @@ namespace BookBloom.Controllers
         public IActionResult Create(Category category) 
         {
             //custom validation
-            //if(category.Name == category.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
-            //}
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
             //model state validation
             if (ModelState.IsValid)
             {
@@ -49,7 +49,51 @@ namespace BookBloom.Controllers
                 return RedirectToAction("Index");
             }
             return View();
-           
+        }
+
+        /// <summary>
+        /// get method for edit
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = dbContext.Category.Find(id);
+            Category? categoryFromDb1 = dbContext.Category.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDb2 = dbContext.Category.Where(u => u.Id == id).FirstOrDefault();
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        /// <summary>
+        /// post method for edit
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            //custom validation
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            //model state validation
+            if (ModelState.IsValid)
+            {
+                dbContext.Category.Add(category);
+                dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
