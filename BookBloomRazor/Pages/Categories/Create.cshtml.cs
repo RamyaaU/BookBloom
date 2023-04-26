32e2555e5e45b8a2c4f5440dc24b74f223720657
Ramya.U <ramyayadav2016@gmail.com>
2023-04-26 21:54:51 +0530

    corrected the redirecttopage view

--- BookBloomRazor/Pages/Categories/Create.cshtml.cs
+++ BookBloomRazor/Pages/Categories/Create.cshtml.cs
@@ -5,10 +5,16 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookBloomRazor.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
         private readonly BookBloomRazorDbContext dbContext;
 
+        ///// <summary>
+        ///// when working on razorpages, if we want teh proprty to be available
+        ///// when working on post method then bindprop is used.
+        ///// </summary>
+        //[BindProperty]
         public Category Category { get; set; }
 
         public CreateModel(BookBloomRazorDbContext razorDbContext)
@@ -16,10 +22,23 @@ namespace BookBloomRazor.Pages.Categories
             dbContext = razorDbContext;
         }
 
+        /// <summary>
+        /// get method for category
+        /// </summary>
         public void OnGet()
         {
             //CategoryList = dbContext.Category.ToList();
         }
-        
+
+        /// <summary>
+        /// post method for category
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult OnPost(Category category)
+        {
+            dbContext.Category.Add(category);
+            dbContext.SaveChanges();
+            return RedirectToPage("Index");
+        }
     }
 }
