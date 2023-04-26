85547fe600ea9a43db3e22601568641290b876b8
Ramya.U <ramyayadav2016@gmail.com>
2023-04-26 16:54:46 +0530

    added model class  and setup of ef for razor project

---
+++ BookBloomRazor/Data/BookBloomRazorDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BookBloomRazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookBloomRazor.Data
+{
+    public class BookBloomRazorDbContext   : DbContext
+    {
+        public BookBloomRazorDbContext(DbContextOptions<BookBloomRazorDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci-fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
--- BookBloomRazor/Program.cs
+++ BookBloomRazor/Program.cs
@@ -1,7 +1,15 @@
+using BookBloomRazor.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+//registering db context
+builder.Services.AddDbContext<BookBloomRazorDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DbConnection")));
+
 
 var app = builder.Build();
 
