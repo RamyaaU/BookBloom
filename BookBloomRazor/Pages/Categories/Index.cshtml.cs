7b76e05233ef413d4ccfcb5ca9a36087c6a13e61
Ramya.U <ramyayadav2016@gmail.com>
2023-04-26 17:49:03 +0530

    adding view, and controller method to razor project to display all categories

--- BookBloomRazor/Pages/Categories/Index.cshtml.cs
+++ BookBloomRazor/Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using BookBloomRazor.Data;
+using BookBloomRazor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,18 @@ namespace BookBloomRazor.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+
+        private readonly BookBloomRazorDbContext dbContext;
+
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(BookBloomRazorDbContext razorDbContext)
+        {
+            dbContext = razorDbContext;
+        }
         public void OnGet()
         {
+            CategoryList = dbContext.Category.ToList();
         }
     }
 }
