bf7e9f9131561a8748ba1fee02fdf0a7a044f1a8
Ramya.U <ramyayadav2016@gmail.com>
2023-04-28 16:16:13 +0530

    implementation of generic repo and base repo for category

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookBloom.Data;
+using BookBloom.DataAccess.Repository.IRepository;
+using BookBloom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBloom.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private BookBloomDbContext dbContext;
+
+        public CategoryRepository(BookBloomDbContext _dbContext) : base(_dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        public void Save()
+        {
+            dbContext.SaveChanges();
+        }
+
+        public void Update(Category category)
+        {
+            dbContext.Category.Update(category);
+        }
+    }
+}
