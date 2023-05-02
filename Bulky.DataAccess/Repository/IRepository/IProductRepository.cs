803bb7d7fc56aa31e8948748e99b86b5f8c91056
Ramya.U <ramyayadav2016@gmail.com>
2023-05-02 12:58:38 +0530

    implementation of product model, repo and unit of work

---
+++ Bulky.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using BookBloom.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBloom.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository
+    {
+
+    }
+}
