993afb09cfb400cfc2ad8d152f8c0dd94d420abd
Ramya.U <ramyayadav2016@gmail.com>
2023-05-03 21:29:46 +0530

    added categoryid fk for product table

--- Bulky.Modes/Models/Product.cs
+++ Bulky.Modes/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -42,5 +43,11 @@ namespace BookBloom.Models.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public string Price100 { get; set; }
+
+        //column of product
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
     }
 }
