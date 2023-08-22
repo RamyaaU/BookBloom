8d8e0fd447fdea4d9fe53c4c9b934bb15ffd3af7
Ramya.U <ramyayadav2016@gmail.com>
2023-08-22 17:17:36 +0530

    implementation of homepage for the app

--- BookBloom/Areas/Customer/Controllers/HomeController.cs
+++ BookBloom/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using BookBloom.Models;
+﻿using BookBloom.DataAccess.Repository;
+using BookBloom.DataAccess.Repository.IRepository;
+using BookBloom.Models;
+using BookBloom.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +11,18 @@ namespace BookBloom.Web.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
